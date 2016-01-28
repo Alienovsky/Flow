59b8bf676c3b06839d91a2af37c37b26e1e519fb
Kamil Trepczynski <ktrepczynski@rule.lan>
2016-01-28 18:00:33 +0100

    added listener

--- src/test/java/com/kamilmade/MainTest.groovy
+++ src/test/java/com/kamilmade/MainTest.groovy
@@ -4,6 +4,7 @@ import com.kamilmade.enums.Events
 import com.kamilmade.enums.States
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.statemachine.StateMachine
 import spock.lang.Specification
@@ -12,8 +13,6 @@ import spock.lang.Specification
 class MainTest extends Specification {
     @Autowired
     StateMachine<States,Events> stateMachine;
-
-
 
     def "test start state machine and move from technical INITIAL state to REGISTERED"(){
         when:
@@ -32,7 +31,13 @@ class MainTest extends Specification {
     def "test move state from REGISTERED to CANCELLED"(){
         given:
         stateMachine.start();
+        stateMachine.getState().getId() == States.INITIAL
+
+        when:
         stateMachine.sendEvent(Events.RUN)
+
+        then:
+        stateMachine.getState().getId() == States.REGISTERED
 
         when:
         stateMachine.sendEvent(Events.CANCEL);
@@ -44,7 +49,7 @@ class MainTest extends Specification {
         stateMachine.sendEvent(Events.FINISH);
 
         then:
-        stateMachine.getState().getId() == States.FINAL;
+        stateMachine.getState() == null;
     }
 
 
